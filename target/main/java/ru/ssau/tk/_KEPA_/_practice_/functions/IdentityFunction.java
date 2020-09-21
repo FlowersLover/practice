4eff2fb51951eb1cce73bff1f404738686d7dde2
Murashkina <grewytri33@gmail.com>
2020-09-21 18:26:12 +0400

    Добавлен класс IdentityFunction, реализующий интерфейс MathFunction

--- target/main/java/ru/ssau/tk/_KEPA_/_practice_/functions/IdentityFunction.java
+++ target/main/java/ru/ssau/tk/_KEPA_/_practice_/functions/IdentityFunction.java
@@ -3,7 +3,7 @@ package ru.ssau.tk._KEPA_._practice_.functions;
 public class IdentityFunction implements MathFunction {
     @Override
     public double apply(double x){
-        return x ;
+        return x;
     }
 
 
