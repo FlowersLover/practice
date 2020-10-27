eb72f023ae16338ed25ab53a1c16e1f2995b121a
Murashkina <grewytri33@gmail.com>
2020-10-27 18:11:31 +0400

    Л.Р 3.1 Добавлен класс Point

---
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk._KEPA_._practice_.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
