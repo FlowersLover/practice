897dda3b080d30bb34a5c8658b6b277e31db1da3
Murashkina <grewytri33@gmail.com>
2020-11-04 21:36:51 +0400

    Л.Р 3.3 Добавлен класс TabulatedFunctionFileWriter .

---
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk._KEPA_._practice_.io;
+
+import ru.ssau.tk._KEPA_._practice_.functions.*;
+
+import java.io.*;
+
+public class TabulatedFunctionFileWriter {
+    public static void main(String[] args) {
+        double[] x = {2.,4.,6.,8.};
+        double[] y = {4.,16.,36.,64.};
+        TabulatedFunction listFunction = new LinkedListTabulatedFunction(x, y);
+        TabulatedFunction arrayFunction = new ArrayTabulatedFunction(x, y);
+        try {
+            BufferedWriter outArray = new BufferedWriter(new FileWriter("output/array function.txt"));
+            BufferedWriter outList = new BufferedWriter(new FileWriter("output/linked list function.txt"));
+
+            FunctionsIO.writeTabulatedFunction(outArray, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(outList, listFunction);
+
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+    }
+}
