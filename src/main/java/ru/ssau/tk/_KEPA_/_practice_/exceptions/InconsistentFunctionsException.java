3b1a8d9d95c8cc3fafcdc3097eaf014d4b908c09
Murashkina <grewytri33@gmail.com>
2020-11-04 19:35:23 +0400

    Л.Р 3.2 Добавлен класс InconsistentFunctionsException

---
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk._KEPA_._practice_.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
+
