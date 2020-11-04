bb9e6c081208ec988d5ffcd664c271d9b718f833
Murashkina <grewytri33@gmail.com>
2020-11-04 21:10:52 +0400

    Л.Р 3.3 Добавлен метод writeTabulatedFunction()

--- src/main/java/ru/ssau/tk/_KEPA_/_practice_/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/io/FunctionsIO.java
@@ -1,7 +1,19 @@
 package ru.ssau.tk._KEPA_._practice_.io;
 
+import java.io.*;
+import ru.ssau.tk._KEPA_._practice_.functions.*;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        writer.flush();
     }
 }
