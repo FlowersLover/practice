301dc8d3b782686b867cbe72073d01e3a1cbf14e
Murashkina <grewytri33@gmail.com>
2020-11-05 07:30:47 +0400

    Л.Р 3.3 Добавлен метод writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function).

--- src/main/java/ru/ssau/tk/_KEPA_/_practice_/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/io/FunctionsIO.java
@@ -1,11 +1,22 @@
 package ru.ssau.tk._KEPA_._practice_.io;
 
 import java.io.*;
+
 import ru.ssau.tk._KEPA_._practice_.functions.*;
 
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream output = new DataOutputStream(outputStream);
+        output.writeInt(function.getCount());
+        for (Point newPoint : function) {
+            output.writeDouble(newPoint.x);
+            output.writeDouble(newPoint.y);
+        }
+        output.flush();
     }
 
     public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
