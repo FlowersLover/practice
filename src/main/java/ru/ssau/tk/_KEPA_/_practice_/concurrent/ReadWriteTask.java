ecd809fc23984375ae35bdc9d78ff911ad64e622
Murashkina <grewytri33@gmail.com>
2020-11-22 22:46:44 +0400

    Л.Р 4.1 созданы классы ReadWriteTask, реализующий интерфейс Runnable, и  ReadWriteTaskExecutor с main-методом.

---
+++ src/main/java/ru/ssau/tk/_KEPA_/_practice_/concurrent/ReadWriteTask.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk._KEPA_._practice_.concurrent;
+
+import ru.ssau.tk._KEPA_._practice_.functions.*;
+
+public class ReadWriteTask implements Runnable {
+    private final TabulatedFunction function;
+
+    ReadWriteTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            double x = function.getX(i);
+            double y;
+            y = function.getY(i);
+            System.out.println(Thread.currentThread().getName() + ", before write: i = " + i + ", x = " + x + ", y = " + y);
+            function.setY(i, y + 1);
+            y = function.getY(i);
+
+            System.out.println(Thread.currentThread().getName() + ", after write: i = " + i + ", x = " + x + ", y = " + y + "\n");
+        }
+    }
+}
